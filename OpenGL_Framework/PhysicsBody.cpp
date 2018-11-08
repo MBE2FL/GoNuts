7b93c6c4902a1403821ef689bbf82692f9d124e2
Kevin <Kevin.lounsbury@uoit.net>
2018-11-08 17:05:08 -0500

    Removed extra comments from collision

--- OpenGL_Framework/PhysicsBody.cpp
+++ OpenGL_Framework/PhysicsBody.cpp
@@ -46,81 +46,29 @@ bool PhysicsBody::collision(PhysicsBody* physicsBody)
 	float otherWidth = otherBounds.getExtends().x;
 	float otherHeight = otherBounds.getExtends().y;
 
-	unsigned int left, right, up, down;
-	left = 0; right = 0; up = 0; down = 0;
-
-	//if (up == 1||down ==1)
-	//	return 1;
-	//else if (left == 1)
-	//	return 2;
-	//else if (right == 1)
-	//	return 3;
-	//else
-	//	return 0;
-
-
 	if (position.x - width < otherPosition.x + otherWidth &&
 		position.x + width > otherPosition.x - otherWidth &&
 		position.y - height < otherPosition.y + otherHeight &&
 		position.y + height > otherPosition.y - otherHeight)
 	{
-		//Vector2 toLeftCorner(otherPosition.x - otherWidth, otherPosition.y + otherHeight);
-		//toLeftCorner = toLeftCorner - otherPosition;
-		//Vector2 toRightCorner(otherPosition.x + otherWidth, otherPosition.y + otherHeight);
-		//toRightCorner = toRightCorner - otherPosition;
-		////std::cout << toCorner << std::endl;
-		//float angleToLeftCorner;
-		//angleToLeftCorner = atanh(toLeftCorner.y / toLeftCorner.x) * 180.0f / PI;
-		//float angleToRightCorner;
-		//angleToRightCorner = atanh(toRightCorner.y / toRightCorner.x) * 180.0f / PI;
-		////std::cout << angleToLeftCorner << std::endl;
-		//Vector2 toPlayer(position - otherPosition);
-		//float angleToPlayer;
-		//angleToPlayer = atanh(toPlayer.y / toPlayer.x) * 180.0f / PI;
-		//std::cout << std::endl;
-		////std::cout << angleToLeftCorner << std::endl;
-		//std::cout << angleToPlayer << std::endl;
-		//std::cout << std::endl;
-		//if (angleToPlayer <= 0.0f)
-		//{
-		//	if (angleToLeftCorner > angleToPlayer)
-		//		return 1;
-		//	else
-		//		return 2;
-		//}
-		//else
-		//{
-		//	if (angleToRightCorner < angleToPlayer)
-		//		return 1;
-		//	else
-		//		return 2;
-		//}
 		setUseGravity(false);
 
-		
-
 		if (position.y - (height * 0.8f) >= otherPosition.y + otherHeight &&
-			position.y - height <= otherPosition.y + otherHeight 
-			)
+			position.y - height <= otherPosition.y + otherHeight)
 		{
-			//if (position.y - height < otherPosition.y + otherHeight)
-			//	position.y = position.y + 0.5f;//(otherPosition.y + otherHeight - position.y - height);
 			if (getVelocity().y <= -0.01f)
 			{
 				float ySpeed = getVelocity().y;
 				addForce(Vector2(0.0f, -ySpeed / dt));
 				
 			}
-			//if (ySpeed < 0.05f && ySpeed > -0.05)
-			//	addForce(Vector2(0.0f, -GRAVITY * 1.002f));
 		}
 		else if (position.x - width < otherPosition.x + otherWidth &&
 			position.x + width > otherPosition.x + otherWidth &&
 			getVelocity().x < -0.1f)
 		{
 			float xSpeed = getVelocity().x;
-			addForce(Vector2(-xSpeed / (dt * 0.6f), 0.0f));
-			
+			addForce(Vector2(-xSpeed / (dt * 0.6f), 0.0f));	
 		}
 		else if (position.x + width > otherPosition.x - otherWidth &&
 			position.x - width < otherPosition.x + otherWidth &&
@@ -128,15 +76,12 @@ bool PhysicsBody::collision(PhysicsBody* physicsBody)
 		{
 			float xSpeed = getVelocity().x;
 			addForce(Vector2(-xSpeed / (dt * 0.6f), 0.0f));
-			
 		}
-		
 		return true;
 	}
 	else
 	{
 		setUseGravity(true);
-		
 		return false;
 	}
 }
