8de9b7c949a92744330a61899811360df2319580
ONCAMPUS\100671306 <max.kemp@uoit.net>
2019-03-25 14:21:27 -0400

    Changed UI to use UIQuad mesh.

--- OpenGL_Framework/Game.cpp
+++ OpenGL_Framework/Game.cpp
@@ -99,7 +99,7 @@ void Game::initializeGame()
 	ObjectLoader::loadMesh("Building Top 3", "./Assets/Models/Building_Top3.obj");
 	ObjectLoader::loadMesh("Building Top 4", "./Assets/Models/Building_Top4.obj");
 
-	ObjectLoader::loadMesh("UIQuad", "./Assets/Models/background.obj");
+	ObjectLoader::loadMesh("UIQuad", "./Assets/Models/UIQuad.obj");
 
 
 
